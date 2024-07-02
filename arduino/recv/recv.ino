5642e771dbc9e98c033717f853581109638b451b
WangGwonYoo <kong3157@gmail.com>
2024-07-02 17:33:13 +0900

    아두이노 commit

---
+++ arduino/recv/recv.ino
@@ -0,0 +1,11 @@
+void setup() {
+  Serial.begin(9600);  // 시리얼 통신 시작
+}
+
+void loop() {
+  // if (Serial.available() > 0) {  // 데이터가 있는지 확인
+  //   String data = Serial.readStringUntil('\n');  // 데이터를 읽고 문자열로 변환
+  //   Serial.print("Received: ");  // 데이터 출력
+  //   Serial.println(data);
+  // }
+}
---
+++ arduino/send/send.ino
@@ -0,0 +1,8 @@
+void setup() {
+  Serial.begin(9600);  // 시리얼 통신 시작
+}
+
+void loop() {
+  // Serial.println("Hello from Arduino 1");  // 데이터 전송
+  // delay(1000);  // 1초 대기
+}
