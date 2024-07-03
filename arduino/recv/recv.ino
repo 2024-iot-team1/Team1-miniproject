ff87fdf6cf1d3dcc99ca079687c1fa27407d9c4b
WangGwonYoo <kong3157@gmail.com>
2024-07-03 11:27:32 +0900

    아두이노간 시리얼 통신 성공

--- arduino/recv/recv.ino
+++ arduino/recv/recv.ino
@@ -1,11 +1,17 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(5, 6); // RX, TX
+
 void setup() {
-  Serial.begin(9600);  // 시리얼 통신 시작
+  Serial.begin(9600);  // 기본 시리얼 통신 시작 (디버깅 용도)
+  mySerial.begin(9600); // SoftwareSerial 통신 시작
 }
 
 void loop() {
-  // if (Serial.available() > 0) {  // 데이터가 있는지 확인
-  //   String data = Serial.readStringUntil('\n');  // 데이터를 읽고 문자열로 변환
-  //   Serial.print("Received: ");  // 데이터 출력
-  //   Serial.println(data);
-  // }
+  if (mySerial.available() > 0) {  // SoftwareSerial 데이터가 있는지 확인
+    String data = mySerial.readStringUntil('\n');  // 데이터를 읽고 문자열로 변환
+    Serial.print("Received: ");  // 시리얼 모니터에 데이터 출력
+    Serial.println(data);
+  }
+  delay(1000);  // 1초 대기
 }
--- arduino/send/send.ino
+++ arduino/send/send.ino
@@ -1,8 +1,14 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(5, 6); // RX, TX
+
 void setup() {
-  Serial.begin(9600);  // 시리얼 통신 시작
+  Serial.begin(9600);  // 기본 시리얼 통신 시작 (디버깅 용도)
+  mySerial.begin(9600); // SoftwareSerial 통신 시작
 }
 
 void loop() {
-  // Serial.println("Hello from Arduino 1");  // 데이터 전송
-  // delay(1000);  // 1초 대기
+  mySerial.println("Hello from Arduino 1");  // SoftwareSerial로 데이터 전송
+  Serial.println("Sent: Hello from Arduino 1"); // 시리얼 모니터에 전송된 데이터 표시
+  delay(1000);  // 1초 대기
 }
