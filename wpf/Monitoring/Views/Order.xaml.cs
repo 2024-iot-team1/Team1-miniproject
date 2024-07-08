a6155c2ee0c109a8138dcdc93ac1930fdb5fb8f5
Hwangji99 <ghkdwl1303@naver.com>
2024-07-08 12:32:42 +0900

    오전 마무리

--- wpf/Monitoring/Views/Order.xaml.cs
+++ wpf/Monitoring/Views/Order.xaml.cs
@@ -38,12 +38,16 @@ namespace Monitoring.Views
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     connection.Open();
-                    // Orders 테이블과 Delivery 테이블을 Inner Join 하여 데이터 가져오는 쿼리
-                    string query = @"SELECT O.OrderNum, O.InventoryNum, O.Quantity, O.OrderDT, D.DeliveryDate
-                                     FROM Orders O
-                                     INNER JOIN Delivery D ON O.OrderNum = D.OrderNum";
 
-                    SqlCommand command = new SqlCommand("SELECT * FROM [Orders]", connection);
+                    // Orders 테이블과 Delivery 테이블을 INNER JOIN하고 Product 테이블도 INNER JOIN하여 데이터 가져오는 쿼리
+                    string query = @"SELECT O.OrderNum, P.ProductCode, P.ProductName, P.Price,
+                                    O.InventoryNum, O.Quantity, O.OrderDT, 
+                                    D.DeliveryNum, D.DeliveryStatus, D.StartDT, D.CompleteDT, D.Destination
+                             FROM Orders O
+                             INNER JOIN Delivery D ON O.OrderNum = D.OrderNum
+                             INNER JOIN Product P ON O.InventoryNum = P.InventoryNum";
+
+                    SqlCommand command = new SqlCommand(query, connection);
                     SqlDataAdapter adapter = new SqlDataAdapter(command);
                     DataTable dataTable = new DataTable();
                     adapter.Fill(dataTable);
@@ -56,5 +60,60 @@ namespace Monitoring.Views
             }
         }
 
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            // 현재 선택된 항목이 있는지 확인
+            if (OrderDataGrid.SelectedItem != null)
+            {
+                // 선택된 항목을 DataRowView로 캐스팅
+                DataRowView selectedRow = (DataRowView)OrderDataGrid.SelectedItem;
+
+                // 배송 상태 열의 값을 "배송 취소"로 변경
+                selectedRow["DeliveryStatus"] = "배송취소";
+
+                // 변경 사항을 데이터 그리드에 반영
+                OrderDataGrid.Items.Refresh();
+            }
+            else
+            {
+                MessageBox.Show("취소할 항목을 선택해주세요.");
+            }
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // 데이터 그리드에서 변경된 데이터를 가져오기
+                    DataTable changedDataTable = ((DataView)OrderDataGrid.ItemsSource).Table;
+
+                    // SqlDataAdapter를 사용하여 변경 사항을 데이터베이스에 반영
+                    SqlDataAdapter adapter = new SqlDataAdapter();
+
+                    // UpdateCommand를 설정하여 변경 사항을 데이터베이스에 반영
+                    string updateQuery = @"UPDATE Delivery
+                                   SET DeliveryStatus = @DeliveryStatus
+                                   WHERE OrderNum = @OrderNum";
+
+                    adapter.UpdateCommand = new SqlCommand(updateQuery, connection);
+                    adapter.UpdateCommand.Parameters.Add("@DeliveryStatus", SqlDbType.NVarChar, 50, "DeliveryStatus");
+                    adapter.UpdateCommand.Parameters.Add("@OrderNum", SqlDbType.Int, 0, "OrderNum");
+
+                    // 변경된 데이터를 SqlDataAdapter에 적용하여 데이터베이스에 업데이트
+                    adapter.Update(changedDataTable);
+
+                    MessageBox.Show("변경 사항이 저장되었습니다.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("변경 사항을 저장하는 동안 오류가 발생했습니다: " + ex.Message);
+            }
+        }
     }
 }
