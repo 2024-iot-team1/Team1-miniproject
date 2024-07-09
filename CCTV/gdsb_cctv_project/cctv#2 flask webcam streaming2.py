6fc7de2bc417c5d7e3b22cef2215883786fa6765
kimdongju1 <dkdlkdh@naver.com>
2024-07-09 09:14:19 +0900

    .

--- CCTV/gdsb_cctv_project/cctv#2 flask webcam streaming2.py
+++ CCTV/gdsb_cctv_project/cctv#2 flask webcam streaming2.py
@@ -20,13 +20,13 @@ def gen_frames():
 
 @app.route('/')
 def index():
-    return render_template('index4#2.html')             # index4#2.html의 형식대로 웹페이지를 보여줌
+    return render_template('index.html')             # index4#2.html의 형식대로 웹페이지를 보여줌
 
 @app.route('/video_feed')
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame') # 그림파일들을 쌓아서 보여줌
 
 if __name__ == "__main__":  # 웹사이트를 호스팅항 접속자에게 보여주기 위한 부분
-   app.run(host="192.168.5.3", port = "18013")
+   app.run(host="192.168.5.3", port = "8080")
    # host는 현재 라즈베리파이의 내부 IP, port는 임의로 설정
    # 해당 내부 IP와 port를 포트포워딩 해두면 외부에서도 접속가능
