e20ca9610b227f48599c26f0cbc18e985068a895
YooWangGwon <kong3157@gmail.com>
2024-07-29 12:55:51 +0900

    push

--- wpf/Monitoring/Views/Setting.xaml.cs
+++ wpf/Monitoring/Views/Setting.xaml.cs
@@ -1,22 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Diagnostics;
-using System.IO;
-using System.Runtime.Remoting.Channels;
 using System.IO.Ports;
-using System.IdentityModel.Protocols.WSTrust;
 
 
 namespace Monitoring.Views
@@ -96,37 +82,56 @@ namespace Monitoring.Views
             }
         }
 
+        // Flask 서버를 시작하는 메서드
         private void StartFlaskServer()
         {
+            // 프로세스 시작 정보를 설정합니다.
             var startInfo = new ProcessStartInfo
             {
+                // 실행 파일 이름을 설정합니다. 여기서는 파이썬 인터프리터를 사용합니다.
                 FileName = "python",
+                // 실행 파일에 전달할 인수를 설정합니다. 여기서는 Flask 서버를 실행하는 파이썬 스크립트의 경로를 지정합니다.
                 Arguments = "\"C:\\Sources\\Team1-miniproject\\CCTV\\gdsb_cctv_project\\cctv#3 flask webcam with text.py\"",
+                // 새 창을 열지 않고 명령줄 창을 숨깁니다.
                 UseShellExecute = false,
+                // 프로세스의 표준 출력을 리디렉션합니다.
                 RedirectStandardOutput = true,
+                // 프로세스의 표준 오류를 리디렉션합니다.
                 RedirectStandardError = true,
+                // 프로세스를 숨깁니다(명령줄 창을 표시하지 않습니다).
                 CreateNoWindow = true,
             };
 
+            // 새로운 프로세스 인스턴스를 생성하고 시작 정보를 설정합니다.
             _flaskProcess = new Process
             {
                 StartInfo = startInfo
             };
 
+            // 프로세스의 표준 출력 데이터를 수신하기 위한 이벤트 핸들러를 설정합니다.
             _flaskProcess.OutputDataReceived += (sender, args) => Debug.WriteLine(args.Data);
+            // 프로세스의 표준 오류 데이터를 수신하기 위한 이벤트 핸들러를 설정합니다.
             _flaskProcess.ErrorDataReceived += (sender, args) => Debug.WriteLine(args.Data);
 
+            // 프로세스를 시작합니다.
             _flaskProcess.Start();
+            // 비동기적으로 표준 출력 읽기를 시작합니다.
             _flaskProcess.BeginOutputReadLine();
+            // 비동기적으로 표준 오류 읽기를 시작합니다.
             _flaskProcess.BeginErrorReadLine();
         }
 
+        // Flask 서버를 중지하는 메서드
         private void StopFlaskServer()
         {
+            // 프로세스가 null이 아니고 종료되지 않은 경우
             if (_flaskProcess != null && !_flaskProcess.HasExited)
             {
+                // 프로세스를 강제 종료합니다.
                 _flaskProcess.Kill();
+                // 프로세스와 관련된 모든 리소스를 해제합니다.
                 _flaskProcess.Dispose();
+                // 프로세스 변수를 null로 초기화합니다.
                 _flaskProcess = null;
             }
         }
